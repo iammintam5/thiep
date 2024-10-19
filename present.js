55dfa4d997ea4b66dc1dfe46cec20e9e72bf64b9
iammintam5 <huynhminhtam8842@gmail.com>
2024-10-19 20:04:00 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,6 +1,6 @@
 
 var to = 'Thu Yến';
-var gift_url = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/460410171_1333593018016630_979809138166011915_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeGqWeQ3gCZZc_X78y0hrH0h5-ZNYcj9_ifn5k1hyP3-JyGy3yQvxdNP0EzM1gQhK40crN5fJmsptOWLGIHsfQC3&_nc_ohc=Io6c4d2gvPgQ7kNvgFWsOXD&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&_nc_gid=AfEuJlOy0hxFSMry-OGTbV5&oh=03_Q7cD1QHxUQfXNGzZCbkNTqyNB9AvYUOlMAbOxTp50Hyc-0TB0Q&oe=673AB81B';
+var gift_url = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/462534238_545829825066953_885417925123900362_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=0024fc&_nc_ohc=H2IgOhSs664Q7kNvgF7Gv7a&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&_nc_gid=ANUIaX4VKSVNyNpEZ96f7SU&oh=03_Q7cD1QEw6eOv70GlAWZCNTo-zNo1GGCO6zw8p3TPrj2Jdwf7Ww&oe=673B036A';
 var gift_image_url = 'wtf.png';
 
 
